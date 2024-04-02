7e9594047431607233c3ae3195f38afc527ff03e
codeofnilesh <147924890+codeofnilesh@users.noreply.github.com>
2024-04-03 00:18:39 +0530

    Update Nilesh.java

--- src/main/java/in/nilesh/demo/Nilesh.java
+++ src/main/java/in/nilesh/demo/Nilesh.java
@@ -4,7 +4,7 @@ public class Nilesh {
 	
 	public void getNilesh() {
 		System.out.println("Nilesh");
-		System.out.println("Hello all nill");
+		System.out.println("Hello all nill pavan  ");
 		
 	}
 
